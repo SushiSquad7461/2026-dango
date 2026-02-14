67b60bd41f8df5b4256b430e4a6661dbcdc07320
AnoushkaGharat <83427391+AnoushkaGharat@users.noreply.github.com>
2026-02-13 22:39:52 -0800

    Updated build.gradle to automatically generate BuildConstants

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-dango";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "c94ebd807377b5c237cd2a0d8349526c8bde0a39";
+  public static final String GIT_DATE = "2026-02-07 12:57:59 PST";
+  public static final String GIT_BRANCH = "swerve";
+  public static final String BUILD_DATE = "2026-02-13 22:29:43 PST";
+  public static final long BUILD_UNIX_TIME = 1771050583975L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
