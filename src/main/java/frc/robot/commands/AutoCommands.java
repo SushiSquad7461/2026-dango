2c963a12c39b32f1a8636f2fc48d52ecb32d5f9a
Sushi Squad <team7461@gmail.com>
2026-02-28 11:58:27 -0800

    added named commands to pathplanner

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -9,6 +9,7 @@ import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.StateMachine.RobotState;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
@@ -23,7 +24,8 @@ public class AutoCommands {
             selectedAuto = autoNetworkTable.getStringTopic("selectedAuto").publish();
             selectedAuto.set("Nothing");
 
-            NamedCommands.registerCommand("Move_Ahead", stateMachine.changeState(null) );
+            NamedCommands.registerCommand("Shoot", stateMachine.changeState(RobotState.SHOOT_ONLY) );
+            NamedCommands.registerCommand("Shoot", stateMachine.changeState(RobotState.INTAKE_DOWN) );
     
             autoChooser.setDefaultOption("Nothing", new InstantCommand());
             autoChooser.addOption("a", new PathPlannerAuto("a"));
