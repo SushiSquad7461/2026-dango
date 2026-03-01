68bf8753e6f1a31bfc769531d24e86dad9aadc4c
Sushi Squad <team7461@gmail.com>
2026-03-01 10:32:45 -0800

    updated intake constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-dango";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 57;
-  public static final String GIT_SHA = "1c543934e178a4bda196d024e8e929b051c05725";
-  public static final String GIT_DATE = "2026-02-28 16:09:39 PST";
+  public static final int GIT_REVISION = 58;
+  public static final String GIT_SHA = "ba12969d9a75c4e2556753dc70583b734cec2d32";
+  public static final String GIT_DATE = "2026-02-28 17:53:19 PST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2026-02-28 17:51:58 PST";
-  public static final long BUILD_UNIX_TIME = 1772329918332L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-28 17:54:37 PST";
+  public static final long BUILD_UNIX_TIME = 1772330077679L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
