1e9fe21c8037492a7cde5b59ef152006d72f8031
LuckyCookie274 <aadrao80@gmail.com>
2026-03-04 19:36:19 -0800

    fixed autoalign thx to claude, untested

--- src/main/java/frc/robot/commands/AutoAlign.java
+++ src/main/java/frc/robot/commands/AutoAlign.java
@@ -1,9 +1,9 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.generated.Constants;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.vision.limelight_vision.Vision;
@@ -12,31 +12,35 @@ public class AutoAlign extends Command {
     private final Swerve swerve;
     private final Vision vision;
     private final boolean isRed;
+    private final PIDController rotationPID;
 
     public AutoAlign(Swerve swerve, Vision vision, boolean isRed) {
         this.swerve = swerve;
         this.vision = vision;
         this.isRed = isRed;
+        rotationPID = new PIDController(0.15, 0, 0);
+        rotationPID.setTolerance(2.0);
+        rotationPID.enableContinuousInput(-180, 180);
         addRequirements(this.swerve);
     }
 
     @Override
     public void execute() {
-        swerve.drive(new Translation2d(0, 0),  vision.getHeadingToScorePillar(isRed).getRadians(), true, true);
+        Rotation2d targetHeading = vision.getHeadingToScorePillar(isRed);
+        double rotation = rotationPID.calculate(
+            swerve.getHeading().getDegrees(),
+            targetHeading.getDegrees()
+        );
+        swerve.drive(new Translation2d(0, 0), rotation, true, true);
     }
 
     @Override
     public boolean isFinished() {
-        Rotation2d targetHeading = vision.getHeadingToScorePillar(isRed);
-        Rotation2d currentHeading = swerve.getHeading();
-        double errorDegrees = Math.abs(targetHeading.minus(currentHeading).getDegrees());
-        //TODO: Tune error degrees if needed
-        return errorDegrees < Constants.Vision.ERROR_DEGREES;
+        return rotationPID.atSetpoint();
     }
 
     @Override
     public void end(boolean interrupted) {
-        // Stop motors
         swerve.drive(new Translation2d(0, 0), 0, true, true);
     }
 }
