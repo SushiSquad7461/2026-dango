efb617c575acc85cbe0367a1f1bbfb1d4300f87a
Sushi Squad <team7461@gmail.com>
2026-03-06 02:01:37 -0800

    IT WORKS(we need to tune for carpet and comp lighting, but code works now)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-dango";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 124;
-  public static final String GIT_SHA = "e3ccdea3a29fe5c70acb33d43d8397307632f1ae";
-  public static final String GIT_DATE = "2026-03-06 02:10:46 EST";
+  public static final int GIT_REVISION = 127;
+  public static final String GIT_SHA = "5dda95e1e58d0c17d29e388c27d92e919381b6e0";
+  public static final String GIT_DATE = "2026-03-06 04:11:07 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2026-03-06 03:04:29 EST";
-  public static final long BUILD_UNIX_TIME = 1772784269504L;
+  public static final String BUILD_DATE = "2026-03-06 04:51:45 EST";
+  public static final long BUILD_UNIX_TIME = 1772790705074L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
