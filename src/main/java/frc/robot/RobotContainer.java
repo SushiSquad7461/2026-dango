87b1a303f75851051c2014230e423fc491f9ff0d
LuckyCookie274 <aadrao80@gmail.com>
2026-03-07 15:14:29 -0800

    small fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -204,7 +204,6 @@ public class RobotContainer {
                 driverController.leftTrigger().whileTrue(new AutoAlign(
                     swerve,
                     vision,
-                    shooter,
                     () -> DriverStation.getAlliance().isPresent() &&
                         DriverStation.getAlliance().get() == DriverStation.Alliance.Red
                 ));
