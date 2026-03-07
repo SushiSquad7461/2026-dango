d392670de0526c91e6e891efc04cf927dad68cfe
Sushi Squad <team7461@gmail.com>
2026-03-07 09:20:16 -0800

    did auto fixes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-dango";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 132;
-  public static final String GIT_SHA = "e1b41772a03516fb544ccdeedb8818048e6cb297";
-  public static final String GIT_DATE = "2026-03-06 13:42:55 EST";
+  public static final int GIT_REVISION = 149;
+  public static final String GIT_SHA = "12afec10cf66805d7e83db7f792bea22fe51ccdc";
+  public static final String GIT_DATE = "2026-03-06 19:31:19 EST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2026-03-06 13:58:44 EST";
-  public static final long BUILD_UNIX_TIME = 1772823524604L;
+  public static final String BUILD_DATE = "2026-03-06 19:32:16 EST";
+  public static final long BUILD_UNIX_TIME = 1772843536273L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -13,13 +14,15 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.StateMachine.RobotState;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
+import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.vision.limelight_vision.Vision;
 
 public class AutoCommands {
     private final SendableChooser<Command> autoChooser=new SendableChooser<Command>();;
     private final StringPublisher selectedAuto;
     private final NetworkTable autoNetworkTable;
 
-    public AutoCommands(StateMachine stateMachine, Intake intake, ShooterSubsystem shooter){
+    public AutoCommands(StateMachine stateMachine, Intake intake, ShooterSubsystem shooter, Swerve swerve, Vision vision){
             autoNetworkTable = NetworkTableInstance.getDefault().getTable("Auto");
             selectedAuto = autoNetworkTable.getStringTopic("selectedAuto").publish();
             selectedAuto.set("Nothing");
@@ -30,7 +33,15 @@ public class AutoCommands {
             new InstantCommand(() -> stateMachine.scheduleNewState(RobotState.IDLE)));
         NamedCommands.registerCommand("Idle",
             new InstantCommand(() -> stateMachine.scheduleNewState(RobotState.IDLE)));
-    
+        NamedCommands.registerCommand("AutoAlign",
+            new AutoAlign(
+                    swerve,
+                    vision,
+                    shooter,
+                    () -> DriverStation.getAlliance().isPresent() &&
+                        DriverStation.getAlliance().get() == DriverStation.Alliance.Red
+                ));
+
             autoChooser.setDefaultOption("Nothing", new InstantCommand());
             autoChooser.addOption("Test_Auto", new PathPlannerAuto("Test_Auto"));
             autoChooser.addOption("B1_Hub_HP", new PathPlannerAuto("B1_Hub_HP"));
