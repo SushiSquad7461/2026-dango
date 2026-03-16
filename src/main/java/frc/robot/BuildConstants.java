ed5acbce66ab26c60f8ac0f86f15fdc333dc5fa4
Sushi Squad <team7461@gmail.com>
2026-03-15 17:50:43 -0700

    Adjusted the shooter numbers according to the new shooter

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-dango";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 159;
-  public static final String GIT_SHA = "81cf242d232113e693b3aece7fbeb167a09512e9";
-  public static final String GIT_DATE = "2026-03-08 11:54:39 EDT";
-  public static final String GIT_BRANCH = "Auto_InatkeDown";
-  public static final String BUILD_DATE = "2026-03-08 13:36:24 EDT";
-  public static final long BUILD_UNIX_TIME = 1772991384238L;
+  public static final int GIT_REVISION = 165;
+  public static final String GIT_SHA = "112b68828bd5ca0c986aacf108eb604039da59be";
+  public static final String GIT_DATE = "2026-03-11 19:50:29 EDT";
+  public static final String GIT_BRANCH = "Main";
+  public static final String BUILD_DATE = "2026-03-15 19:06:36 EDT";
+  public static final long BUILD_UNIX_TIME = 1773615996509L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
