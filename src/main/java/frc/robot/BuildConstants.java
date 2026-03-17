739a10958d98192b720241788d5ef5c1125d2872
Sushi Squad <team7461@gmail.com>
2026-03-16 23:24:47 -0700

    no longer crashes when reading april tags

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-dango";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 174;
-  public static final String GIT_SHA = "8454e9cca5f9268313fbc63e1bf5fc6b437993a0";
-  public static final String GIT_DATE = "2026-03-16 22:32:09 EDT";
+  public static final int GIT_REVISION = 175;
+  public static final String GIT_SHA = "342357152705ef015914346f4f812daced3946ec";
+  public static final String GIT_DATE = "2026-03-16 22:47:54 EDT";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2026-03-16 22:45:44 EDT";
-  public static final long BUILD_UNIX_TIME = 1773715544268L;
+  public static final String BUILD_DATE = "2026-03-17 02:07:01 EDT";
+  public static final long BUILD_UNIX_TIME = 1773727621109L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
