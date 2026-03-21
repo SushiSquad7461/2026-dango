ec8e4b471293d3998fba8b5106d8af4565bdcc3c
Sushi Squad <team7461@gmail.com>
2026-03-21 14:08:08 -0700

    Sammamish comp day1: tuned constants

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -16,7 +16,7 @@ public class Intake extends SubsystemBase{
     
     public enum IntakeState {
             IDLE(false, 0,IntakeConstants.stowedAngleDeg),
-            DEPLOYED(true, 0.35,IntakeConstants.intakeAngleDeg),
+            DEPLOYED(true,Constants.IntakeConstants.rollerSpeed,IntakeConstants.intakeAngleDeg),
             WIGGLING(true, 0,80);
 
             public final boolean intakeExtended;
