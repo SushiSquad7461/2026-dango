46e46807c60cd65afeb282225304f9825c98ec95
Sushi Squad <team7461@gmail.com>
2026-03-22 08:08:39 -0700

    Tuned shottable

--- src/main/java/frc/robot/generated/Constants.java
+++ src/main/java/frc/robot/generated/Constants.java
@@ -147,7 +147,7 @@ public class Constants {
         /** Meters per Second */
         public static final double maxSpeed = 4.5;
         /** Radians per Second */
-        public static final double maxAngularVelocity = 10.0;
+        public static final double maxAngularVelocity = 7.2;
 
         /* Neutral Modes */
         public static final NeutralModeValue angleNeutralMode = NeutralModeValue.Coast;
