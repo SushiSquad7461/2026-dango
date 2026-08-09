e77558092944def22640159df4338d8ed3b6c834
originalhawk242 <oghawk242@gmail.com>
2026-08-09 12:10:49 -0700

    deleted hopper and intake, robotcontainer and statemachine are still left

--- src/main/java/frc/robot/subsystems/hopper/HopperIO.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.hopper;
-
-import frc.robot.subsystems.hopper.Hopper.HopperState;
-
-public interface HopperIO {
-    void changeState(HopperState newState);
-    void setSpeed(double speed);
-
-}
