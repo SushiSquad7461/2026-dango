5407e78d8bb9d49f835cb57b62559c57fad3fef5
originalhawk242 <oghawk242@gmail.com>
2026-09-25 16:15:27 -0700

    added mt2 in Swerve.java

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -14,7 +14,6 @@ import frc.robot.commands.StateMachine.RobotState;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
-import frc.robot.subsystems.vision.Vision;
 
 public class AutoCommands {
     private final SendableChooser<Command> autoChooser=new SendableChooser<Command>();;
