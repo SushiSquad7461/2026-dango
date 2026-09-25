fb51e0e59755efaac8cbc4cfa6b194cbbd0e409e
AnoushkaGharat <83427391+AnoushkaGharat@users.noreply.github.com>
2026-09-25 16:22:29 -0700

    combined the shooterhw and hoodhw in shooter class

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,4 +1,50 @@
 package frc.robot.subsystems.shooter;
 
-public class Shooter {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.generated.Constants;
+
+public class Shooter extends SubsystemBase {
+    private final ShooterHW flywheel;
+    private final HoodHW hood;
+    private double targetRPM = 0.0;
+
+    public Shooter(ShooterHW flywheel, HoodHW hood) {
+        this.flywheel = flywheel;
+        this.hood = hood;
+    }
+
+    public void setShot(double rpm, double hoodDegrees) {
+        targetRPM = rpm;
+        flywheel.setRPM(rpm);
+        hood.setPosition(hoodDegrees);
+    }
+
+    public void stop() {
+        targetRPM = 0.0;
+        flywheel.stopShooter();
+        flywheel.setFeeder(false);
+    }
+
+    public boolean isReady() {
+        return targetRPM > 0
+                && Math.abs(flywheel.getRPM() - targetRPM) < Constants.Shooter.SHOOTER_RPM_TOLERANCE;
+    }
+
+    public Command shoot(double rpm, double hoodDegrees) {
+        return runOnce(() -> setShot(rpm, hoodDegrees))
+                .andThen(Commands.waitUntil(() -> isReady()))
+                .andThen(runOnce(() -> flywheel.setFeeder(true)))
+                .andThen(Commands.idle())
+                .finallyDo(() -> stop());
+    }
+
+    public Command stepHood(double deltaDegrees) {
+        return runOnce(() -> hood.stepHood(deltaDegrees));
+    }
+
+    public Command zeroHood() {
+        return runOnce(() -> hood.zeroHood());
+    }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterHW.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterHW.java
@@ -1,100 +1,90 @@
 package frc.robot.subsystems.shooter;
 
-
 import com.ctre.phoenix6.CANBus;
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.*;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import frc.robot.generated.Constants;
 
-class ShooterHW implements ShooterIO {
-    CANBus rioCanBus = new CANBus("rio");
+public class ShooterHW implements ShooterIO {
+    private final CANBus rioCanBus = new CANBus("rio");
 
-    // initialize shooter, shooter intake, and hood motors
     private final TalonFX krakenShooterLeft = new TalonFX(14, rioCanBus);
     private final TalonFX krakenShooterRight = new TalonFX(2, rioCanBus);
     private final TalonFX krakenShooterFeeder = new TalonFX(5, rioCanBus);
-    private final VelocityVoltage shooterRequest = new VelocityVoltage(0).withSlot(0); // create a velocity closed-loop request, voltage output, slot 0 configs
-    private final VelocityVoltage feederRequest = new VelocityVoltage(0).withSlot(0);
 
-    ShooterHW() {
-        // shooter flywheel motor config
+    private final VelocityVoltage shooterRequest = new VelocityVoltage(0).withSlot(0);
+    private final VelocityVoltage feederRequest = new VelocityVoltage(0).withSlot(0);
+    private final NeutralOut neutral = new NeutralOut();
+
+    public ShooterHW() {
         TalonFXConfiguration shooterConfig = new TalonFXConfiguration();
         shooterConfig.CurrentLimits.StatorCurrentLimit = 120.0;
         shooterConfig.CurrentLimits.StatorCurrentLimitEnable = true;
         shooterConfig.CurrentLimits.SupplyCurrentLimit = 70.0;
         shooterConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
         shooterConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-        // adapts for different battery voltages
+        shooterConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
         shooterConfig.Voltage.PeakForwardVoltage = 12.0;
         shooterConfig.Voltage.PeakReverseVoltage = -12.0;
+        shooterConfig.Slot0.kS = Constants.Shooter.SHOOTER_KS;
+        shooterConfig.Slot0.kV = Constants.Shooter.SHOOTER_KV;
+        shooterConfig.Slot0.kP = Constants.Shooter.SHOOTER_KP;
+        shooterConfig.Slot0.kI = Constants.Shooter.SHOOTER_KI;
+        shooterConfig.Slot0.kD = Constants.Shooter.SHOOTER_KD;
         krakenShooterLeft.getConfigurator().apply(shooterConfig);
-        // sets second shooter motor w same config, opposite direction
+        krakenShooterRight.getConfigurator().apply(shooterConfig);
         krakenShooterRight.setControl(new Follower(krakenShooterLeft.getDeviceID(), MotorAlignmentValue.Opposed));
-        // setup PID
-        Slot0Configs shooterPID = new Slot0Configs();
-        shooterPID.kS = Constants.Shooter.SHOOTER_KS;
-        shooterPID.kV = Constants.Shooter.SHOOTER_KV;
-        shooterPID.kP = Constants.Shooter.SHOOTER_KP;
-        shooterPID.kI = Constants.Shooter.SHOOTER_KI;
-        shooterPID.kD = Constants.Shooter.SHOOTER_KD;
-        krakenShooterLeft.getConfigurator().apply(shooterPID);
-        krakenShooterRight.getConfigurator().apply(shooterPID);
 
-        // shooter intake wheel motor config
-        TalonFXConfiguration shooterFeederConfig = new TalonFXConfiguration();
-        shooterFeederConfig.CurrentLimits.StatorCurrentLimit = 50.0;
-        shooterFeederConfig.CurrentLimits.StatorCurrentLimitEnable = true;
-        shooterFeederConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
-        shooterFeederConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-        shooterFeederConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-        shooterFeederConfig.Voltage.PeakForwardVoltage = 12.0;
-        shooterFeederConfig.Voltage.PeakReverseVoltage = -12.0;
-        krakenShooterFeeder.getConfigurator().apply(shooterFeederConfig);
-
-        // setup PID
-        Slot0Configs shooterFeederPID = new Slot0Configs();
-        shooterFeederPID.kS = Constants.Shooter.FEEDER_KS;
-        shooterFeederPID.kV = Constants.Shooter.FEEDER_KV;
-        shooterFeederPID.kP = Constants.Shooter.FEEDER_KP;
-        shooterFeederPID.kI = Constants.Shooter.FEEDER_KI;
-        shooterFeederPID.kD = Constants.Shooter.FEEDER_KD;
-        krakenShooterFeeder.getConfigurator().apply(shooterFeederPID);
+        TalonFXConfiguration feederConfig = new TalonFXConfiguration();
+        feederConfig.CurrentLimits.StatorCurrentLimit = 50.0;
+        feederConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+        feederConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
+        feederConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        feederConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        feederConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        feederConfig.Voltage.PeakForwardVoltage = 12.0;
+        feederConfig.Voltage.PeakReverseVoltage = -12.0;
+        feederConfig.Slot0.kS = Constants.Shooter.FEEDER_KS;
+        feederConfig.Slot0.kV = Constants.Shooter.FEEDER_KV;
+        feederConfig.Slot0.kP = Constants.Shooter.FEEDER_KP;
+        feederConfig.Slot0.kI = Constants.Shooter.FEEDER_KI;
+        feederConfig.Slot0.kD = Constants.Shooter.FEEDER_KD;
+        krakenShooterFeeder.getConfigurator().apply(feederConfig);
     }
-    //sets shooter motor rpm
+
     @Override
     public void setRPM(double rpm) {
-        double rps = rpm / 60.0;
-        krakenShooterLeft.setControl(shooterRequest.withVelocity(-rps));
+        krakenShooterLeft.setControl(shooterRequest.withVelocity(rpm / 60.0));
     }
-    //gets shooter motor rpm
+
     @Override
     public double getRPM() {
         return krakenShooterLeft.getVelocity().getValueAsDouble() * 60.0;
     }
-    //stops shooter by coasting to 0, rather than setting 0rpm and the shooter braking
+
     @Override
     public void stopShooter() {
-        krakenShooterLeft.set(0);
+        krakenShooterLeft.setControl(neutral);
     }
-    //feeder is on/off
+
     @Override
     public void setFeeder(boolean on) {
-        double rps = Constants.Shooter.FEEDER_RPM / 60;
-        if(on) {
-            krakenShooterFeeder.setControl(feederRequest.withVelocity(-rps));
+        if (on) {
+            krakenShooterFeeder.setControl(feederRequest.withVelocity(Constants.Shooter.FEEDER_RPM / 60.0));
         } else {
-            krakenShooterFeeder.setControl(feederRequest.withVelocity(0));
+            krakenShooterFeeder.setControl(neutral);
         }
     }
-    //check if feeder is running
+
     @Override
     public boolean getFeeder() {
-        return krakenShooterFeeder.getVelocity().getValueAsDouble() > 2; //margin of error of 120rpm/2rps
+        return Math.abs(krakenShooterFeeder.getVelocity().getValueAsDouble()) > 2.0;
     }
 }
